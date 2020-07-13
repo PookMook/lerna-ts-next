909dc9f87aaa3bd913da2a9a68127a5c22cd5552
Arthur Juchereau <PookMook@users.noreply.github.com>
2020-07-12 23:38:37 -0400

    Update index.tsx

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { meaningOfLife } from "@welbi/foo";
 
 export default function Index() {
-  return <p>Hello world: {meaningOfLife}</p>;
+  return <p>Hello world, this is a test: {meaningOfLife}</p>;
 }
